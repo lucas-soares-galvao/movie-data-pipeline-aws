309b8f8649ec93d5439d4b4ac519c9799dbbfbf3
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-05-16 17:16:40 -0300

    teste

--- test/lambda_api/conftest.py
+++ test/lambda_api/conftest.py
@@ -3,4 +3,6 @@ import os
 
 # Adiciona app/lambda_api ao sys.path para que os imports relativos (from src.utils)
 # dentro de main.py funcionem durante os testes, assim como funcionam no Lambda.
+sys.modules.pop("src.utils", None)
+sys.modules.pop("src", None)
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "app", "lambda_api"))
