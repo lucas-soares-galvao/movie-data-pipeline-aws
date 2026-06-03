66a5c7b13ab593326415cdcd48fbc28e7191f8d2
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-06-02 22:54:28 -0300

    teste

--- infra/locals.tf
+++ infra/locals.tf
@@ -39,10 +39,12 @@ locals {
   lambda_api_build_path               = "${path.module}/.lambda_build"
   glue_etl_src_path                   = "${path.root}/../app/${var.glue_etl_path_app}"
   glue_etl_requirements_path          = "${path.root}/../app/${var.glue_etl_path_app}/requirements.txt"
+  glue_etl_wheel_build_path           = "${path.module}/.glue_etl_build"
   glue_data_quality_src_path          = "${path.root}/../app/${var.glue_data_quality_path_app}"
   glue_data_quality_requirements_path = "${path.root}/../app/${var.glue_data_quality_path_app}/requirements.txt"
   glue_agg_src_path                   = "${path.root}/../app/${var.glue_agg_path_app}"
   glue_agg_requirements_path          = "${path.root}/../app/${var.glue_agg_path_app}/requirements.txt"
+  glue_agg_wheel_build_path           = "${path.module}/.glue_agg_build"
   glue_etl_additional_python_modules = join(",", [
     for line in split("\n", file(local.glue_etl_requirements_path)) : trimspace(line)
     if trimspace(line) != "" && !startswith(trimspace(line), "#")
