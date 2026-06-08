44b9e4c497d3c2b5925a3da1703402b22329daed
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-06-08 00:12:45 -0300

    teste

--- app/glue_details/main.py
+++ app/glue_details/main.py
@@ -30,12 +30,13 @@ from src.utils import (
     trigger_data_quality,
 )
 
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    force=True,
+)
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-if not logger.handlers:
-    _h = logging.StreamHandler(sys.stdout)
-    _h.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
-    logger.addHandler(_h)
 
 
 def main() -> None:
